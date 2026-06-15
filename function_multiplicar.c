486ac398e5e7fa5d026e60b4f5184e46e24485ae
ana111222333 <ana.beatriz3217890@gmail.com>
2026-06-15 12:47:54 -0300

    Update function_multiplicar.c

--- function_multiplicar.c
+++ function_multiplicar.c
@@ -2,26 +2,9 @@ int multiplicar(int a, int b) {
 
     int resultado = 0;
 
-    for(int i = 0; i < b; i++) {
+    for (int i = 0; i < b; i++) {
         resultado += a;
     }
 
     return resultado;
-}
-
-int dividir(int a, int b) {
-
-    if (b == 0) {
-        printf("Erro: divisao por zero.\n");
-        return 0;
-    }
-
-    int contador = 0;
-
-    while (a >= b) {
-        a -= b;
-        contador++;
-    }
-
-    return contador;
 }
